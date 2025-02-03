3502a61c98c8d5667bfe76cd221267c0995133c8
Jorge Duarte <jorgeduarte22@gmail.com>
2025-02-03 01:59:37 +0100

    Add retrocompatibility for synonyms

--- main.js
+++ main.js
@@ -190,6 +190,10 @@ function drawMap() {
 	});
 }
 
+function getSynonym(municipio) {
+	return municipios[provincia][municipio].synonym || municipio
+}
+
 function drawMunicipiosList() {
 	clearMunicipiosList();
 	getSortedMunicipios().forEach(m => {
@@ -235,6 +239,13 @@ function loadState() {
 			state = INITIAL_STATE;
 		}
 	}
+
+	// TODO: For retrocompatibility. Delete at some point
+	state.spain = state.spain
+		.map(m => {
+			return getSynonym(m)
+		});
+
 	saveState();
 }
 
